261b96f10816866710469121190dae21520b80c5
Eric <ericcui@qq.com>
2024-08-11 21:43:34 +0800

    add code

---
+++ SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ChoseFileBtn_Clicked(object sender, EventArgs e)
+        {
+            var result = await FilePicker.Default.PickAsync();
+        }
+    }
+
+}
